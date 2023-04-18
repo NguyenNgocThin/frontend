9eb8f0280b5d9a536d56b424bf2aae59340e9b98
Thin <thinmax2000@gmail.com>
2023-04-18 17:48:25 +0700

    thin

--- react-ui/src/views/utilities/KHACHHANG.js
+++ react-ui/src/views/utilities/KHACHHANG.js
@@ -9,6 +9,12 @@ import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import SaveIcon from '@mui/icons-material/Save';
 import CancelIcon from '@mui/icons-material/Close';
 
+import TextField from '@mui/material/TextField';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+
+
 import {
     GridRowModes,
     DataGridPro,
@@ -21,9 +27,51 @@ import {
     randomUpdatedDate,
     randomId,
 } from '@mui/x-data-grid-generator';
-import { style } from '@mui/system';
-// document.getElementsByClassName("MuiDataGrid-main")[0].style.display=none;
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
+// const data=[];
+var request = require('request');
+// var optionslogin = {
+//   'method': 'POST',
+//   'url': 'https://44.193.210.107/api/v1/auth/login',
+//   'headers': {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify({
+//     "username": "root",
+//     "password": "root"
+//   })
+
+// };
+// request(optionslogin, function (error, response) {
+//   if (error) throw new Error(error);
+
+//         const token=JSON.parse(response.body);
+
+//         var optionss = {
+//         'method': 'GET',
+//         'url': 'https://44.193.210.107/api/v1/khachhang',
+//         'headers': {
+//             'Authorization': 'Bearer '+token.access_token
+//         }
+//         };
+//         request(optionss, function (error, response) {
+//         if (error) throw new Error(error);
+//         const data=JSON.parse(response.body);
+//         console.log(data.data);
+//         });
+// });
+// const [initialRows, setRows] = React.useState(data);
 
 const initialRows = [
     {
@@ -83,39 +131,152 @@ const initialRows = [
 
 ];
 
+const options = ['Bậc 1', 'Bậc 2', 'Bậc 3', 'Bậc 4', 'Bậc 5'];
 function EditToolbar(props) {
     const { setRows, setRowModesModel } = props;
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+   
+    // const [value, setValue] = React.useState(dayjs('2023-04-04'));
 
+    const [value1, setValue1] = React.useState(options[0]);
+    const [inputValue1, setInputValue1] = React.useState('');
     const handleClick = () => {
-        //  khi click vao nut them hien ra form dien thong tin de them du lieu
-        //keywork add laf form create reactjs
-        //nhap du lieu them va luu 
-        //khi luu du lieu se chay ajax them du lieu ben t3 (thang tai)
-
-        const id = randomId();
-        setRows((oldRows) => [...oldRows, { id, name: '', age: '', isNew: true }]);
-        setRowModesModel((oldModel) => ({
-            ...oldModel,
-            [id]: { mode: GridRowModes.Edit, fieldToFocus: 'id' },
-        }));
     };
+    const Add = () => {
+     
+       var ma_kh=document.getElementById("ma_kh").value;
+       var ten_kh=document.getElementById("ten_kh").value;
+       var dia_chi=document.getElementById("dia_chi").value;
+       var dt=document.getElementById("dt").value;
+       var cmnd=document.getElementById("cmnd").value;
+       console.log("ma kh",ma_kh);
+       console.log("ten_kh",ten_kh);
+       console.log("dia_chi",dia_chi);
+       console.log("dt",dt);
+       console.log("cmnd",cmnd);
 
+    var request = require('request');
+      var optionslogin = {
+        'method': 'POST',
+        'url': 'https://44.193.210.107/api/v1/auth/login',
+        'headers': {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "username": "root",
+          "password": "root"
+        })
+      };
+  
+      request(optionslogin, function (error, response) {
+        if (error) throw new Error(error);
+        const token=JSON.parse(response.body);
+        var options = {
+            'method': 'POST',
+            'url': 'https://44.193.210.107/api/v1/khachhang/add',
+            'headers': {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer '+token.access_token
+            },
+            body: JSON.stringify({
+              "ma_kh":ma_kh,
+              "ten_kh":ten_kh,
+              "dia_chi":dia_chi,
+              "dt":dt,
+              "cmnd":cmnd
+            })
+          
+          };
+          request(options, function (error, response) {
+            if (error) throw new Error(error);
+            console.log(response.body);
+          });
+        
+      });
+        setOpen(false)};
     return (
         <GridToolbarContainer>
-            <Button color="primary" >
-                THÊM KHÁCH HÀNG
-            </Button>
+            <Button onClick={handleOpen}>Thêm Khách Hàng</Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h3" component="h2" textAlign={'center'}>
+                        Nhập thông khách hàng mới
+                    </Typography>
+
+                    <TextField variant='outlined' label="Mã khách hàng" name="ma_kh" id="ma_kh" sx={{ marginTop: 2, height: 60, width: '100%' }} />
+                    <TextField variant='outlined' label="Tên khách hàng" name="ten_kh" id="ten_kh" sx={{ height: 60, width: '100%' }} />
+                    <TextField variant='outlined' label="Số điện thoại" name="dt" id="dt" sx={{ height: 60, width: '100%' }} />
+                    <TextField variant='outlined' label="Số căn cước" name="cmnd" id="cmnd" sx={{ height: 60, width: '100%' }} />
+                    <TextField variant='outlined' label="Địa chỉ" name="dia_chi" id="dia_chi" sx={{ height: 60, width: '100%' }} />
+                    
+
+                    <Button onClick={Add} sx={{ marginTop: '2rem', marginLeft: '25%', height: 50, width: '50%', bgcolor: '#ede7f6', border: '1px solid #000' }}>Thêm</Button>
+                </Box>
+            </Modal>
         </GridToolbarContainer>
     );
 }
+
+
 
 EditToolbar.propTypes = {
     setRowModesModel: PropTypes.func.isRequired,
     setRows: PropTypes.func.isRequired,
 };
 
-function Typography() {
-    const [rows, setRows] = React.useState(initialRows);
+export default function FullFeaturedCrudGrid() {
+  
+       
+
+    const [rows, setRows] = React.useState([]);
+      
+    function fetchDataFromServer() {
+      var request = require('request');
+      var optionslogin = {
+        'method': 'POST',
+        'url': 'https://44.193.210.107/api/v1/auth/login',
+        'headers': {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "username": "root",
+          "password": "root"
+        })
+      };
+  
+      request(optionslogin, function (error, response) {
+        if (error) throw new Error(error);
+        const token=JSON.parse(response.body);
+  
+        var optionss = {
+          'method': 'GET',
+          'url': 'https://44.193.210.107/api/v1/khachhang',
+          'headers': {
+            'Authorization': 'Bearer '+token.access_token
+          }
+        };
+  
+        request(optionss, function (error, response) {
+          if (error) throw new Error(error);
+          const data=JSON.parse(response.body);
+          setRows(data.data); // cập nhật dữ liệu cho Grid
+        });
+      });
+    }
+  
+    // lấy dữ liệu khi component được render
+    React.useEffect(() => {
+      fetchDataFromServer();
+    }, []);
+  
+    
     const [rowModesModel, setRowModesModel] = React.useState({});
 
     const handleRowEditStart = (params, event) => {
@@ -128,13 +289,51 @@ function Typography() {
 
     const handleEditClick = (id) => () => {
         setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+        console.log(rowModesModel);
     };
 
     const handleSaveClick = (id) => () => {
+        
         setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-    };
+        // console.log(rowModesModel[id]);
+//   const updatedRow = rows.find(row => row.id === id);
+//   setUpdatedRows([...updatedRows, updatedRow]);
+// console.log(updatedRow);
 
+    };
+    
     const handleDeleteClick = (id) => () => {
+        console.log(id);
+        var request = require('request');
+      var optionslogin = {
+        'method': 'POST',
+        'url': 'https://44.193.210.107/api/v1/auth/login',
+        'headers': {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "username": "root",
+          "password": "root"
+        })
+      };
+  
+      request(optionslogin, function (error, response) {
+        if (error) throw new Error(error);
+        const token=JSON.parse(response.body);
+  
+        var request = require('request');
+            var options = {
+            'method': 'DELETE',
+            'url': 'https://44.193.210.107/api/v1/khachhang/delete/'+id,
+            'headers': {
+                'Authorization': 'Bearer '+token.access_token
+            }
+            };
+        request(options, function (error, response) {
+        if (error) throw new Error(error);
+        console.log(response.body);
+        });
+      });
         setRows(rows.filter((row) => row.id !== id));
     };
 
@@ -162,35 +361,35 @@ function Typography() {
 
     const columns = [
         {
-            field: 'id',
+            field: 'ma_kh',
             headerName: 'ID',
             width: 180,
             editable: true
         },
         {
-            field: 'name',
+            field: 'ten_kh',
             headerName: 'Tên Khách Hàng',
-            type: 'String',
+            // type: 'String',
             editable: true
         },
         {
-            field: 'sdt',
+            field: 'dt',
             headerName: 'Số Điện Thoại',
-            type: 'string',
+            // type: 'string',
             width: 180,
             editable: true,
         },
         {
-            field: 'cccd',
+            field: 'cmnd',
             headerName: 'Số CCCD',
-            type: 'string',
+            // type: 'string',
             width: 220,
             editable: true,
         },
         {
-            field: 'address',
+            field: 'dia_chi',
             headerName: 'Địa Chỉ',
-            type: 'string',
+            // type: 'string',
             width: 220,
             editable: true,
         },
@@ -271,7 +470,7 @@ function Typography() {
         </Box>
     );
 }
-export default Typography;
+
 
 
 
